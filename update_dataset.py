339a212087377b46b2d1f25242903dd98182a417
George Ho <19851673+eigenfoo@users.noreply.github.com>
2023-01-14 11:48:43 -0500

    Initial commit

---
+++ update_dataset.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+if __name__ == "__main__":
+    # Load inspection data: both historical and for today.
+    historical_inspections = pd.read_csv("data/inspections.csv")
+    new_inspections = pd.read_csv(
+        "https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD"
+    )
+
+    today = pd.to_datetime("today").normalize()
+    new_inspections["first_seen_datetime"] = today
+    new_inspections["last_seen_datetime"] = today
+
+    # Concatenate both datasets, keeping only new rows.
+    subset = list(historical_inspections.columns)
+    subset.remove("first_seen_datetime")
+    subset.remove("last_seen_datetime")
+    combined_inspections = pd.concat(
+        [historical_inspections, new_inspections]
+    ).drop_duplicates(subset=subset, keep="first")
+
+    combined_inspections.to_csv("data/inspections.csv", index=False)
