6e18d24568761b148dd8e3ea066050b760b20897
George Ho <19851673+eigenfoo@users.noreply.github.com>
2023-03-11 16:03:39 -0500

    Add basic logging

--- update_dataset.py
+++ update_dataset.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 if __name__ == "__main__":
     # Load inspection data: both historical and for today.
+    print("Loading historical data...")
     historical_inspections = pd.read_csv(
         "data/inspections.csv.gz",
         parse_dates=["first_seen_datetime", "last_seen_datetime"],
     )
+    print("Fetching today's data...")
     new_inspections = pd.read_csv(
         "https://data.cityofnewyork.us/api/views/43nn-pn8j/rows.csv?accessType=DOWNLOAD"
     )
@@ -15,6 +17,7 @@ if __name__ == "__main__":
     new_inspections["first_seen_datetime"] = today
     new_inspections["last_seen_datetime"] = today
 
+    print("Updating historical data...")
     # Concatenate both datasets, keeping only new rows.
     subset = list(historical_inspections.columns)
     subset.remove("first_seen_datetime")
@@ -23,4 +26,5 @@ if __name__ == "__main__":
         [historical_inspections, new_inspections]
     ).drop_duplicates(subset=subset, keep="first")
 
+    print("Writing...")
     combined_inspections.to_csv("data/inspections.csv.gz", index=False)
